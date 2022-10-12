870278d4af74cb6a0dca56c9aede9f1723c3b644
Alex <rmo84791@gmail.com>
2022-10-12 15:25:44 +0300

    Update 06_who's_paying.py

--- 06_who's_paying.py
+++ 06_who's_paying.py
@@ -7,5 +7,5 @@ names = namesAsCSV.split(", ")
 num_items = len(names)
 
 random_choise = random.randint(0, num_items - 1)
-print(names[random_choise])
+print(names[random_choise] + " is going to buy the meal today!")
   
