19275ddc2661bf6776ff69ac93b6f163cb934a7f
Alex <rmo84791@gmail.com>
2022-10-12 15:23:48 +0300

    who's paying

---
+++ 06_who's_paying.py
@@ -0,0 +1,11 @@
+import random
+test_seed =int(input("Create a seed number: "))
+random.seed(test_seed)
+
+namesAsCSV = input("Give me everbody's names, separated by a coma. ")
+names = namesAsCSV.split(", ")
+num_items = len(names)
+
+random_choise = random.randint(0, num_items - 1)
+print(names[random_choise])
+  
